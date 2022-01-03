f1cbb8d7ec3a99fb23ce4e15445705b236579fb3
diegoor40 <d.rincon@rankup.ai>
2022-01-03 12:35:11 -0600

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const SDK_VERSION: string = '2.3.8'
+export const BEACON_VERSION: string = '2'
---
+++ src/types/beacon/NetworkType.ts
@@ -0,0 +1,10 @@
+export enum NetworkType {
+  MAINNET = 'mainnet',
+  DELPHINET = 'delphinet',
+  EDONET = 'edonet',
+  FLORENCENET = 'florencenet',
+  GRANADANET = 'granadanet',
+  HANGZHOUNET = 'hangzhounet',
+  IDIAZABALNET = 'idiazabalnet',
+  CUSTOM = 'custom'
+}
