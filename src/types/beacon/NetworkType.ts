6a26b635dc06246f0f1cbf7062b2760773c1c04f
diegoor40 <d.rincon@rankup.ai>
2022-01-04 08:39:33 -0600

    d

--- src/types/beacon/NetworkType.ts
+++
@@ -1,8 +0,0 @@
-export enum NetworkType {
-  MAINNET = 'mainnet',
-  DELPHINET = 'delphinet',
-  EDONET = 'edonet',
-  FLORENCENET = 'florencenet',
-  GRANADANET = 'granadanet',
-  CUSTOM = 'custom'
-}
